ebdd7937090550ddf530cc39e789bef1b9361a6e
Tegmore223 <82901784+Tegmore223@users.noreply.github.com>
2024-06-16 15:27:07 +0300

    Update VoiceAssist.py

--- VoiceAssist.py
+++ VoiceAssist.py
@@ -1,22 +1,25 @@
 import speech_recognition
 import webbrowser
 
-webbrowser.register('chrome', None,webbrowser.BackgroundBrowser("C:/Program Files/Google/Chrome/Application/chrome.exe"))
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C:/Program Files/Google/Chrome/Application/chrome.exe"))
 
 sr = speech_recognition.Recognizer()
-sr.pause_threshold=0.5 # создаем паузу, после которой ассистент примет нашу команду
-
-
-commands_dict = {'commands':{
-'search_for_information_on_google':
-['искать', 'гугл', 'найди' ,'найти', 'вики', 'википедия']}}
+sr.pause_threshold = 0.5  # создаем паузу, после которой ассистент примет нашу команду
+sr.energy_threshold = 1500  # устанавливаем более высокий порог энергии
 
 def main():
-    query = listen_comand()
+    while True:
+        query = listen_comand()
 
-    for k, v in commands_dict['commands'].items():
-        if query in v:
-            print(globals()[k]())
+        for google in ['искать', 'гугл', 'найди', 'найти']:
+            if google in query:
+                search_on_google()
+
+        for wiki in ['вики', 'википедия']:
+            if wiki in query:
+                search_on_wiki()
+
+
 
 def listen_comand():
     try:
@@ -25,9 +28,11 @@ def listen_comand():
             audio = sr.listen(source=mic)
             query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
         print(query)
+        return query
 
     except speech_recognition.UnknownValueError:
         return 'Я не понял что вы сказали'
+
 def search_on_google():
     print('что надо найти?')
     search_term = listen_comand()
@@ -40,18 +45,12 @@ def search_on_google():
 def search_on_wiki():
     print('Что именно вы хотите найти в Википедии? ')
     search_term = listen_comand()
+    print(search_term)
 
     url = f"https://ru.wikipedia.org/wiki/{search_term}"
     webbrowser.get('chrome').open_new_tab(url)
 
     return 'Открываю'
 
-
 if __name__ == "__main__":
     main()
-    command = listen_comand()
-    #if command == 'гугл':
-    #    search_on_google()
-    #elif command == 'вики':
-    #    search_on_wiki()
-    #ТИПО ТАКОГО
